01e57f8694633d3e8764a563e53480884046431a
Urmas Öövel <uoovel@itcollege.ee>
2023-09-07 14:18:05 +0300

    Kontrollerid autotestitud

--- script.sh
+++ script.sh
@@ -14,6 +14,7 @@ url[1]="http://localhost:8080/"
 url[2]="http://localhost:8080/koikOsalejad/1"
 url[3]="http://localhost:8080/vormOsaleja/1"
 url[4]="http://localhost:8080/vaataOsalust/1"
+url[5]="http://localhost:8080/newevent"
 
 
 
@@ -61,9 +62,23 @@ else
 	echo "Vigu pole, kood: $status_code -> "$value
 fi
 
+
+#POST
+value="http://localhost:8080/save"
+
+catch response_data status_code curl -ks -w "%{stderr}%{http_code}" -H "Content-type: application/x-www-form-urlencoded"  -d "nimetus=Maamess&aeg=2024-06-26 09:00:00&koht=Tartu"  -X POST   $value
+if [ "$status_code" != "302" ]; then
+	echo -e "\e[31mError: $status_code -> "$value
+
+else
+	echo "Vigu pole, kood: $status_code -> "$value
+fi
+
+
 #DELETE
 
 urldel[1]="http://localhost:8080/deleteOsalus/2"
+urldel[2]="http://localhost:8080/deleteYritus/2"
 
 for valdel in "${urldel[@]}"
 do
